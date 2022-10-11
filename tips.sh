f1668127b7e11f8a241cbc19651717282dbac4b3
Alex Ruger <alexrugermusic@gmail.com>
2022-10-11 14:52:16 -0700

    Added files for Cote and Hartov

---
+++ tips.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# -----------
+# git
+# -----------
+# git add
+# git commit -m "commit message"
+# git push
+#
+# git pull
+#
+# -----------
+# bash
+# -----------
+# cd = change directory
+# ls = list files
+# rm = remove file
+# rm -rf = remove directory
